6d9866a21bb604d74190cb5ec02cf27151750e96
Matias Gonzalez <mng_epson@hotmail.com>
2023-10-20 16:04:49 -0300

    feat: add tailwind config

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,17 @@
 import {Home, PostForm, NotFound} from './pages/index'
 
-import {Routes} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
 function App() {
   return (
-    <div>App</div>
+    <div className='bg-neutral-900 min-h-screen flex items-center'>
+     <div className='px-10 container m-auto'>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/new' element={<PostForm/>}/>
+      <Route path='*' element={<NotFound/>}/>
+    </Routes>
+     </div>
+    </div>
   )
 }
 export default App
