652138ec8c499e88653c04877bf9a1734bc352c3
Matias Gonzalez <mng_epson@hotmail.com>
2023-10-20 11:58:39 -0300

    feat: add front folder

---
+++ front/src/App.js
@@ -0,0 +1,9 @@
+import {Home, PostForm, NotFound} from './pages/index'
+
+import {Routes} from 'react-router-dom'
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+export default App
--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -1,3 +1,3 @@
-export { default as HomePage } from './HomePage';
-export { default as NotFound } from './NotFound';
-export { default as PostForm } from './PostForm';
+export { Home } from './Home';
+export { NotFound } from './NotFound';
+export { PostForm } from './PostForm';
